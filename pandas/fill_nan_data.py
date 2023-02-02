7aef5b5ef8b1b707a4e108ddd480f5159a569e19
Ahmed AbdElmaksoud <ahmed.abdelmaksoud@sedd.gov.ae>
2023-02-02 11:50:20 +0400

    drop and fill data
    drop and fill data

---
+++ pandas/fill_nan_data.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Feb  2 10:52:43 2023
+
+@author: 459
+"""
+
+import pandas as pd
+
+
+
+import numpy as np 
+
+import time
+
+
+
+#pip install missingno
+#import missingno as msno
+
+path = ".\\test_csv_drop.csv"
+#df data frame
+df = pd.read_csv(path,encoding='cp1252')
+
+#drop column
+#df = df.drop(columns=["Customer Service Agent", "Company"])
+
+#print(" header " , df.columns)
+print( "null values  " , df["age"].isna())
+
+
+
+
+print("df info after  drop " ,df.info())
+
+# drop null rows based on column
+#df = df.dropna(subset= ["age"], axis= 0)
+df = df.fillna(df ["age"].mean())
+#fill null data of age with mean
+print("df info after  drop " ,df.info())
