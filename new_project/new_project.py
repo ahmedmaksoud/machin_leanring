61d6868a20636a4cdf1ebe1e3b7d5645eaaad878
ahmedmaksoud <a.elmaksoud@gmail.com>
2023-02-07 20:30:43 +0400

    adding new jupyter

---
+++ new_project/new_project.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  7 17:26:47 2023
+
+@author: 459
+"""
+
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import  seaborn as sns
+
+path = ".\\housing.csv"
+#df data frame
+df = pd.read_csv(path,encoding='cp1252')
+x = 5
+x
+df.head()
+
+
